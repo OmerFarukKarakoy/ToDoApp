c6fe73d30e1341ffd1c40bdf86e2d628b2fc271a
Ömer Faruk Karaköy <156546037+OmerFarukKarakoy@users.noreply.github.com>
2024-06-02 18:09:03 +0300

    Add files via upload

---
+++ Todo.style.js
@@ -0,0 +1,37 @@
+import {Dimensions,StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: '#1B2631',
+    },
+ 
+
+    headerText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color:'orange',
+      },
+
+    inputBottom:{
+      flex:1,
+    },
+
+    List:{
+      flex:1,
+    },
+
+    TodoContainer:{
+      border:1,
+    },
+    divider: {
+      width: '100%',
+      height: 1,
+      backgroundColor: '#ccc',
+      marginVertical: 10,
+    },
+    
+  
+
+});
